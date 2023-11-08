2186421f11c7178410587dc0d79667e775339b78
arthraw <arthursallesandrade@gmail.com>
2023-11-08 00:30:50 -0300

    feat: New colors for UI

--- app/src/main/java/com/project/petproject/viewmodel/use_case/util/AcceptanceRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.project.petproject.viewmodel.use_case.util
-
-sealed class AcceptanceRequest(val requestType: AcceptanceRequestType) {
-    class Send (requestType: AcceptanceRequestType): AcceptanceRequest(requestType)
-    class NotSend (requestType: AcceptanceRequestType): AcceptanceRequest(requestType)
-}
--- app/src/main/java/com/project/petproject/viewmodel/use_case/util/AcceptanceRequestType.kt
+++
@@ -1,8 +0,0 @@
-package com.project.petproject.viewmodel.use_case.util
-
-sealed class AcceptanceRequestType {
-    object isAccepted: AcceptanceRequestType()
-    object isSend: AcceptanceRequestType()
-    object isNotSend: AcceptanceRequestType()
-    object isNotAccepted: AcceptanceRequestType()
-}
