ca6242f8eacc15a0a52362628608c2826134a77c
arthraw <arthursallesandrade@gmail.com>
2023-12-19 22:00:13 -0300

    build: Dependencies update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.project.petproject"
         minSdk = 24
+        //noinspection ExpiredTargetSdkVersion
         targetSdk = 32
         versionCode = 1
         versionName = "1.0"
@@ -99,14 +100,14 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation ("androidx.navigation:navigation-compose:2.7.4")
-    implementation ("com.google.dagger:hilt-android:2.38.1")
+    implementation("com.google.dagger:hilt-android:2.48")
+    annotationProcessor("com.google.dagger:hilt-compiler:2.48")
     implementation ("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
     implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.compose.ui:ui-text-google-fonts:1.5.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("com.google.dagger:hilt-android:2.39.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
